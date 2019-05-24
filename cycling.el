a4c9381c02d14652e58728b558442ef642df8d6d
Ran Wang <wang1hf@uwindsor.ca>
2019-05-24 19:23:06 -0400

    cycling major mode initial commit

---
+++ cycling.el
@@ -0,0 +1,30 @@
+;;; cycling.el --- A cyclist's major-mode in Emacs -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019- 2019
+
+;; Author: Ran Wang
+;; URL:
+;; Package-Version:
+;; Version:
+;; Package-Requires:
+;; Keywords: cycling, training, organizing
+
+;; This file is _not_ part of GNU Emacs
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+
+;;; Code:
